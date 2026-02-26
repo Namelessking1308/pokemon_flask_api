fc9f0609241a1a9b2fc9fcbd64c6296bc040e57c
Namelessking1308 <ibrahimfakir485@gmail.com>
2026-02-26 16:05:30 +0100

    Mise à jour du projet avec backend Flask et frontend Angular

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,26 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PokemonsComponent } from './pages/pokemons/pokemons.component';
-import { BattleComponent } from './pages/battle/battle.component';
-import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PokemonsComponent,
-    BattleComponent,
-    PokemonCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
